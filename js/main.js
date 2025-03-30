31207d31edd65c75ffc984ff58922c74a7ac642a
Youssef <ytelmalwany@gmail.com>
2025-03-30 04:14:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,10 @@ if (localStorage.getItem("name") !== null && localStorage.getItem("password") !=
 
 let inp_name = document.querySelector("#name")
 let inp_password = document.querySelector("#password")
-let check = document.querySelector("[type = 'checkbox']")
 let submit = document.querySelector(`[type = "submit"]`)
 
 submit.addEventListener("click" ,(e) => {
-    if(inp_name.value !== "" && inp_password.value !== "" && check.checked) {
+    if(inp_name.value !== "" && inp_password.value !== "") {
         e.preventDefault()
         location.href = "login.html";
         localStorage.setItem("name", inp_name.value);
